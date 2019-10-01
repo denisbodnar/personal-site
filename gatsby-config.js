0b8c23aa20af9064c4559c061bc4e07262f1505a
Denis Bodnar <denis.bodnar@gmail.com>
2019-10-01 17:12:33 +0300

    minor cahnges

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `this is fine`,
+        short_name: `this is fine`,
         start_url: `/`,
         background_color: `#6b37bf`,
         theme_color: `#6b37bf`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,18 @@ import Layout from "../components/layout"
 
 export default ({ data }) => (
   <Layout>
-    <h4>
-      A personal blog about living and coding daily
-    </h4>
-    <hr/>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <Link
             to={node.fields.slug}
           >
             <h3>
-              {node.frontmatter.title}{" "} — {node.frontmatter.date}
+              {node.frontmatter.title}
             </h3>
           </Link>
+            <h5>
+              {node.frontmatter.date}
+            </h5>
             <p>{node.excerpt}</p>
         </div>
       ))}
